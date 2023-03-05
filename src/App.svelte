15a2e77259816456f35133fc64961e1ce6e2e2d6
Thomas Meyer <thomas.myr.dev@gmail.com>
2023-03-05 22:39:13 +0100

    refactor: export blob background to delegated svelte component

--- src/App.svelte
+++ src/App.svelte
@@ -1,60 +1,7 @@
 <script>
-    import {blob} from "./main.js";
-
-    document.body.onpointermove = event => {
-        const {clientX, clientY} = event;
-
-        blob.animate({
-            left: `${clientX}px`,
-            top: `${clientY}px`,
-        }, {duration: 3000, fill: "forwards"});
-    }
-
+    import BlobBackground from "./lib/BlobBackground.svelte";
 </script>
 
 <main>
-    <div id="blur-blob"></div>
-    <div id="blob"></div>
+    <BlobBackground/>
 </main>
-
-<style>
-    #blob {
-        height: 400px;
-        width: 400px;
-        aspect-ratio: 1;
-        position: absolute;
-        left: 50%;
-        top: 50%;
-        translate: -50% -50%;
-        border-radius: 50%;
-
-        background: white linear-gradient(
-                to right,
-                aquamarine,
-                rebeccapurple
-        );
-
-        animation: rotate 20s infinite;
-    }
-
-    @keyframes rotate {
-        from {
-            rotate: 0deg;
-        }
-        50% {
-            scale: 1 1.5;
-        }
-        to {
-            rotate: 360deg;
-        }
-    }
-
-    #blur-blob {
-        height: 100%;
-        width: 100%;
-        position: absolute;
-        z-index: 2;
-        backdrop-filter: blur(140px);
-    }
-
-</style>
