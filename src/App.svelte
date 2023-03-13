7be9ec7c8a35b16821b7802c980e24ccc41847bc
Thomas Meyer <thomas.myr.dev@gmail.com>
2023-03-13 20:29:13 +0100

    refactor: export background color to variable

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,8 @@
     import BlobBackground from "./lib/BlobBackground.svelte";
     import Home from "./lib/Home.svelte";
     import StarsBackground from "./lib/StarsBackground.svelte";
+
+    document.documentElement.style.cssText = "--background-color:#0c0c0c;"
 </script>
 
 <main>
